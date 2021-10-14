81151920deb418bf741d48c5178c1922f3a5d2f0
BenHadman <benhadman@gmail.com>
2021-10-14 20:00:44 +0100

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,134 @@
+const path = require("path");
+const { components } = require("./src/plugins/components");
+
+module.exports = {
+  title: "LinkedIn Growth Tool",
+  tagline: "Extend LinkedIn Ads",
+  url: "",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "Jeffail",
+  projectName: "benthos",
+  customFields: {
+    components: {
+      inputs: components("inputs"),
+      processors: components("processors"),
+      conditions: components("conditions"),
+      outputs: components("outputs"),
+      caches: components("caches"),
+      rate_limits: components("rate_limits"),
+      buffers: components("buffers"),
+      metrics: components("metrics"),
+      tracers: components("tracers")
+    }
+  },
+  themeConfig: {
+    prism: {
+      theme: require("./src/plugins/prism_themes/github"),
+      darkTheme: require("./src/plugins/prism_themes/monokai")
+    },
+    colorMode: {
+      defaultMode: "light"
+    },
+    image: "img/og_img.png",
+    metadatas: [{ name: "twitter:card", content: "summary" }],
+    navbar: {
+      title: "Benthos",
+      logo: {
+        alt: "Benthos Blobfish",
+        src: "img/logo.svg"
+      },
+      items: [
+        { to: "docs/about", label: "Docs", position: "left" },
+        { to: "cookbooks", label: "Cookbooks", position: "left" },
+        { to: "blog", label: "Blog", position: "left" },
+        { to: "videos", label: "Videos", position: "left" },
+        { to: "community", label: "Community", position: "right" },
+        {
+          href: "https://github.com/Jeffail/benthos/releases/latest",
+          position: "right",
+          className: "header-download-link header-icon-link",
+          "aria-label": "Download"
+        },
+        {
+          href: "https://github.com/Jeffail/benthos",
+          position: "right",
+          className: "header-github-link header-icon-link",
+          "aria-label": "GitHub repository"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Getting started",
+              to: "docs/guides/getting_started"
+            },
+            {
+              label: "Videos",
+              to: "videos"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Join the chat",
+              to: "community"
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Jeffail/benthos"
+            }
+          ]
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Get in touch",
+              to: "community"
+            },
+            {
+              label: "Mascot",
+              to: "blobfish"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ashley Jeffs.`
+    },
+    algolia: {
+      apiKey: "358e5d3135579871ceecd50c6cb7ce9e",
+      indexName: "benthos"
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/Jeffail/benthos/edit/master/website/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        },
+        blog: {
+          feedOptions: {
+            type: "all"
+          }
+        }
+      }
+    ]
+  ],
+  plugins: [
+    path.resolve(__dirname, "./src/plugins/cookbooks"),
+    path.resolve(__dirname, "./src/plugins/redirects")
+  ]
+};
