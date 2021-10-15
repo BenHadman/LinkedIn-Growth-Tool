44452a9dbb69c5e195ffd33c7c91de8f927a7e3a
BenHadman <benhadman@gmail.com>
2021-10-15 11:58:51 +0100

    gds

--- sidebars.js
+++ sidebars.js
@@ -15,20 +15,10 @@ module.exports = {
       type: "doc",
       id: "intro"
     },
-
     {
       type: "category",
-      label: "Components",
-      items: [
-        //"components/about",
-        //"components/http/about",
-        {
-          type: "category",
-          label: "Google Sheets & AppScript",
-          items: "google_sheets_appscript"
-        },
-        "google_sheets_appscript"
-      ]
+      label: "Background",
+      items: ["google_sheets_appscript"]
     } /*
     {
       type: "category",
